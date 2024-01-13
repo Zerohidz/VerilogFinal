a2394c91b94389b0d3b196504ee07e8c601ec7f4
Dursun Zahid Korkmaz <dursunzahidkorkmaz@gmail.com>
2024-01-13 15:30:50 +0300

    75 puanlık ARTAU_state_fast

--- ARTAU.v
+++ ARTAU.v
@@ -16,10 +16,7 @@ module ARTAU(
     parameter NULL = 1_000_000;
     parameter LIGHT_SPEED = 300_000_000;
 
-    // bools
-    reg switch_to_listening = 0; 
-    reg switch_to_assessing = 0; 
-    reg switch_to_emitting = 0; 
+    reg [1:0] ARTAU_state_fast = 2'b00;
 
     reg time_clk = 0;
     integer pulse_count = 0;
@@ -38,6 +35,7 @@ module ARTAU(
         distance_to_target = 0;
         threat_detected = 0;
         ARTAU_state = 0;
+        ARTAU_state_fast = 0;
         pulse_count = 0;
         pulse_emmision_started_at = NULL;
         listen_to_echo_started_at = NULL;
@@ -45,109 +43,64 @@ module ARTAU(
     end
 
     always @(posedge CLK) begin
+        ARTAU_state <= ARTAU_state_fast;
+    end
+
+    always @(time_clk) begin
         if (RST) begin
             radar_pulse_trigger = 0;
             distance_to_target = 0;
             threat_detected = 0;
-            ARTAU_state = 0;
+            ARTAU_state_fast = 0;
             pulse_count = 0;
             pulse_emmision_started_at = NULL;
             listen_to_echo_started_at = NULL;
             status_update_timer_started_at = NULL;
         end
         else begin
-            case (ARTAU_state)
+            case (ARTAU_state_fast)
                 2'b00, 2'b11: begin
-                    if (switch_to_emitting) begin
-                        switch_to_emitting = 0;
-                        ARTAU_state <= 2'b01;
-                    end
-                end
-                2'b01: begin
-                    if (switch_to_listening) begin
-                        switch_to_listening = 0;
-                        ARTAU_state = 2'b10;
-                    end
-                end
-                2'b10: begin
-                    if ($time - listen_to_echo_started_at >= 2000) begin
-                        listen_to_echo_started_at = NULL;
-                        ARTAU_state <= 2'b00;
-
-                        radar_pulse_trigger = 0;    
-                        distance_to_target = 0;
-                        threat_detected = 0;
-                        ARTAU_state = 0;
-                        pulse_count = 0;
-                        pulse_emmision_started_at = NULL;
-                        listen_to_echo_started_at = NULL;
-                        status_update_timer_started_at = NULL;
-                    end
-                    if (switch_to_assessing) begin
-                        switch_to_assessing = 0;
-                        ARTAU_state <= 2'b11;
-                    end
-                    if (switch_to_emitting) begin
-                        switch_to_emitting = 0;
-                        ARTAU_state <= 2'b01;
-                    end
-                end
-                2'b11: begin
-                    if (($time - status_update_timer_started_at) >= 3000 & scan_for_target == 0) begin
-                        status_update_timer_started_at = NULL;
-                        ARTAU_state <= 2'b00;
-
-                        radar_pulse_trigger = 0;
-                        distance_to_target = 0;
-                        threat_detected = 0;
-                        ARTAU_state = 0;
-                        pulse_count = 0;
-                        pulse_emmision_started_at = NULL;
-                        listen_to_echo_started_at = NULL;
-                        status_update_timer_started_at = NULL;
-                    end
-                end
-            endcase
-        end
-    end
-
-    always @(time_clk) begin
-        case (ARTAU_state)
-            2'b00, 2'b11: begin
-                if (scan_for_target) begin
-                    if (switch_to_emitting == 0) begin
+                    if (scan_for_target) begin
                         if (pulse_count > 0)
                             distance_to_target = LIGHT_SPEED * ($time - listen_to_echo_started_at) / 2;
                         radar_pulse_trigger = 1;
                         pulse_emmision_started_at = $time;
                         pulse_count = pulse_count + 1;
-                        switch_to_emitting = 1;
+                        ARTAU_state_fast <= 2'b01;
                     end
                 end
-            end
-            2'b01: begin
-                if (($time - pulse_emmision_started_at) >= 300) begin
-                    if (switch_to_listening == 0) begin
+                2'b01: begin
+                    if (($time - pulse_emmision_started_at) >= 300) begin
                         pulse_emmision_started_at = NULL;
                         radar_pulse_trigger = 0;
                         listen_to_echo_started_at = $time;
-                        switch_to_listening = 1;
+                        ARTAU_state_fast = 2'b10;
                     end
                 end
-            end
-            2'b10: begin
-                if (radar_echo & pulse_count == 1) begin
-                    if (switch_to_emitting == 0) begin
+                2'b10: begin
+                    if ($time - listen_to_echo_started_at >= 2000) begin
+                        listen_to_echo_started_at = NULL;
+                        ARTAU_state_fast <= 2'b00;
+
+                        radar_pulse_trigger = 0;    
+                        distance_to_target = 0;
+                        threat_detected = 0;
+                        ARTAU_state = 0;
+                        ARTAU_state_fast = 0;
+                        pulse_count = 0;
+                        pulse_emmision_started_at = NULL;
+                        listen_to_echo_started_at = NULL;
+                        status_update_timer_started_at = NULL;
+                    end
+                    else if (radar_echo & pulse_count == 1) begin
                         if (pulse_count > 0)
                             distance_to_target = LIGHT_SPEED * ($time - listen_to_echo_started_at) / 1_000_000 / 2;
                         radar_pulse_trigger = 1;
                         pulse_emmision_started_at = $time;
                         pulse_count = pulse_count + 1;
-                        switch_to_emitting = 1;
+                        ARTAU_state_fast <= 2'b01;
                     end
-                end
-                else if (switch_to_emitting == 0 & radar_echo & pulse_count == 2) begin
-                    if (switch_to_assessing == 0) begin
+                    else if (radar_echo & pulse_count == 2) begin
                         old_distance_to_target = distance_to_target;
                         distance_to_target = LIGHT_SPEED * ($time - listen_to_echo_started_at) / 1_000_000 / 2;
                         if (distance_to_target + jet_speed * ($time - listen_to_echo_started_at) / 1_000_000 < old_distance_to_target)begin
@@ -157,14 +110,27 @@ module ARTAU(
                         end
                         status_update_timer_started_at = $time;
                         pulse_count = 0;
-                        switch_to_assessing = 1;
+                        ARTAU_state_fast <= 2'b11;
                     end
                 end
-            end
-            2'b11: begin
+                2'b11: begin
+                    if (($time - status_update_timer_started_at) >= 3000 & scan_for_target == 0) begin
+                        status_update_timer_started_at = NULL;
+                        ARTAU_state_fast <= 2'b00;
 
-            end
-        endcase
+                        radar_pulse_trigger = 0;
+                        distance_to_target = 0;
+                        threat_detected = 0;
+                        ARTAU_state = 0;
+                        ARTAU_state_fast = 0;
+                        pulse_count = 0;
+                        pulse_emmision_started_at = NULL;
+                        listen_to_echo_started_at = NULL;
+                        status_update_timer_started_at = NULL;
+                    end
+                end
+            endcase
+        end
     end
 
 endmodule
